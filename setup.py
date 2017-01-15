27241c2b30277296bfb37bbf28ba9497d4a1b32c
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-01-15 17:54:33 -0300

    bugfix

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 import os
 from setuptools import find_packages, setup
-from pip.req import parse_requirements
 
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session='hack')
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
 
 
 # allow setup.py to be run from any path
@@ -21,7 +13,9 @@ setup(
     name='easy-report',
     version='0.1',
     packages=find_packages(),
-    install_requires=reqs,
+    install_requires=[
+        'reportlab==3.3.0'
+    ],
     include_package_data=True,
     license='MIT',
     description='Ease report for devs',
