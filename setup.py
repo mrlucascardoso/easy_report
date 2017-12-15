34da82bedfc51b5d74e723a832d10e3003dc0dfc
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-12-15 14:21:43 -0300

    Update versão

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='easy-report',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'reportlab==3.3.0'
